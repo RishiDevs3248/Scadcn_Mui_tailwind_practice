45aff9944943229fd09c6dad515984f20e8d146a
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-08-27 20:13:24 +0530

    material ui started

--- src/Mui.jsx
+++ src/Mui.jsx
@@ -1,10 +1,32 @@
+import Button from '@mui/material/Button';
+
 export default function MUI() {
     return (
         <>
-            <div id="nav">
-                <div className="flexContainer left"></div>
+            <div id="nav" className='border-b border-gray-300 flex justify-between p-[10px]'>
+                <div className="flexContainer left font-bold text-xl ">
+                    <Button size="small" variant="contained" color="success"
+                        sx={{
+                            boxSizing:'border-box',
+                            width:"40px",
+                            height:"40px",
+                            marginRight:"10px",
+                        }}
+                    >
+                        M
+                    </Button>
+                    MUI Mastery
+                </div>
                 <div className="flexContainer mid"></div>
-                <div className="flexContainer right"></div>
+                <div className="flexContainer right">
+                    <Button variant="contained" color="success"
+                        sx={{
+                            height:"40px "
+                        }}
+                    >
+                        Success
+                    </Button>
+                </div>
             </div>
         </>
     )
