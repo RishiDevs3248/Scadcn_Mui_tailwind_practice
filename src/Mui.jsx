3ac8c86a5bd55b53961df12d298510bd1aec3da6
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-08-30 18:24:51 +0530

    Button and btn grp

--- src/Mui.jsx
+++ src/Mui.jsx
@@ -1,33 +1,38 @@
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 export default function MUI() {
     return (
         <>
-            <div id="nav" className='border-b border-gray-300 flex justify-between p-[10px]'>
-                <div className="flexContainer left font-bold text-xl ">
-                    <Button size="small" variant="contained" color="success"
-                        sx={{
-                            boxSizing:'border-box',
-                            width:"40px",
-                            height:"40px",
-                            marginRight:"10px",
-                        }}
-                    >
-                        M
-                    </Button>
-                    MUI Mastery
-                </div>
-                <div className="flexContainer mid"></div>
-                <div className="flexContainer right">
-                    <Button variant="contained" color="success"
-                        sx={{
-                            height:"40px "
-                        }}
-                    >
-                        Success
-                    </Button>
-                </div>
+            <div>Mui buttons</div>
+
+
+            <div>Basic Button
+                <Button variant="contained" color="success">
+                    Success
+                </Button></div>
+
+            <div>custom buttons
+                <Button size="small" variant="contained" color="success"
+                    sx={{
+                        boxSizing: 'border-box',
+                        width: "40px",
+                        height: "40px",
+                        marginRight: "10px",
+                    }}
+                >
+                    custom button
+                </Button></div>
+
+            <div>
+                button group
+                <ButtonGroup variant="contained" aria-label="Basic button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
             </div>
+
         </>
     )
 }
